350df8c0c235dc4fd99c7064f1f6075cdf3ab262
eddy <edwardoblette@gmail.com>
2019-11-17 21:33:03 +0100

    debut du graph

---
+++ exo1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+	std::vector<int> next_node;
+	int value;
+}
+class  graph{
+	node new_node;
+
+	public:
+		graph(new_node);
+	
+}
+
+int main()
+{
+	
+	//to do
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,11 @@
+CC=g++
+CFLAGS= -Wall -std=c++17
+LINK=
+
+all:
+
+	$(CC) $(LINK) $(CFLAGS) -o exo1 exo1.cpp
+clear:
+	rm exo1.o ./exo1
+
+
