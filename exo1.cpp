fe89100ce0295328192b118afe815a0f03c45a4a
eddy <edwardoblette@gmail.com>
2019-11-17 23:12:13 +0100

    version non concluente - partir sur list de struct

--- exo1.cpp
+++ exo1.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
+#include <unistd.h>
+#include <vector>
 using namespace std;
 
-struct node{
-	std::vector<int> next_node;
-	int value;
-}
-class  graph{
-	node new_node;
+
+class  node{
+	private:
+		int _id;
+		std::vector<node> _V;
+
 
 	public:
-		graph(new_node);
-	
-}
+		node(int id){
+			_id = id;
+		}
+	//operator
+		node &operator[] (int index){
+			return _V[index];
+		}
+	//setter
+		void add_node(int id){
+			node new_node(id);
+			_V.push_back(new_node);
+		}
+	//getter
+ 		void get_node(){
+ 			std::cout << this->_id << std::endl;
+ 			for (auto it = this->_V.cbegin(); it != this->_V.cend(); it++){
+				std::cout << "->" << it->_id << std::endl;
+			}
+
+ 		}
+};
 
 int main()
 {
-	
+	node root(1);
+	root.add_node(2);
+	root[1].add_node(3);
+	root.get_node();
+
 	//to do
 	return 0;
 }
