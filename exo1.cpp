7d6376005e48250c3329e0483717f374f1112d65
eddy <edwardoblette@gmail.com>
2019-12-05 15:06:47 +0100

    Commentaire sur le fichier principal

--- exo1.cpp
+++ exo1.cpp
@@ -1,3 +1,12 @@
+/**
+    * Exo voiture
+    * Machine Parallele
+    * @teacher Mehdi Boucheta <boucheta.mehdi@gmail.com>
+    *
+    * @autor Edgar Oblette <edwardoblette@gmail.com>
+    *
+    * 20/11/2019
+    */
 #include <iostream>
 #include <unistd.h>
 #include <vector>
@@ -11,9 +20,9 @@
 #define CARS THREAD
 
 // PROTO
-void commounLink_adding(uint, uint);
 void graph_init();
 void car_init();
+void commounLink_adding(uint, uint);
 void car_moving(uint);
 void mutex_securing(uint, uint);
 void treading();
@@ -24,44 +33,47 @@ std::vector<car> vehicules;
 std::mutex global_mutex;
 
 
-void commounLink_adding(uint a, uint b){
- if((a < root.size()) && ( b < root.size()))
-	root[a].add_link(&root[b]);
-	root[b].add_link(&root[a]);
-}
-
-void mutex_securing(uint oldLocation, uint newLocation){
-	root[newLocation].set_arriving();
-	root[oldLocation].set_leaving();
-}
-
+ /**
+ 	* @desc Initalise le graph avec des noeud
+ */
 void graph_init(){
+	 // liste de ville 
 	std::vector<std::string> list{"lyon", "paris", "bordeaux", "marseille"};//"lille", "reims", "grenoble", "rouen", "annecy", "montpellier", "nantes", "montellimar"};
-	//cree node
+	
+	 // cree noeud
 	int i = 0;
 	for (auto it = list.cbegin(); it != list.cend(); it++, ++i)
 				root.push_back(node(*it, i));
 
-	//cree lien
+	 // cree une "route" avec tout les autres villes
 	for(int villeA = 0; villeA < root.size(); ++villeA){
 		for(int villeB = 0; villeB< root.size(); ++villeB){
 			root[villeA].add_link(&root[villeB]);
 		}
 	}		
-	
+	 
+	 // affiche 3 noeud pour tester si tout s'est bien initialiser
 	root[0].get_node();
 	root[1].get_node();
 	root[2].get_node();
 }
 
+ /**
+ 	* @desc Initalise les voitures
+ */  
 void car_init(){
 	for(int i = 0 ; i < CARS; i++){
+
+		 // place les voiture dans une ville
 		uint newLocation;
 		do{
 			newLocation = (rand() % root.size()) ;
 		}while(root[newLocation].isAvailable() == false);
 		
+		 // ajoute un objet voiture dans le vecteur 'vehicule'
 		vehicules.push_back(car(newLocation, "Volvo"));
+
+		 // met le status de la ville où la voiture est initialiser en 'indisponible'
 		root[newLocation].set_availability(false);
 		std::cout << "Voiture [" << i << "] " << root[newLocation].get_cityName() << std::endl;
 
@@ -69,10 +81,34 @@ void car_init(){
 
 }
 
+ /**
+ 	* @desc ajoute un lien entre une ville A et une ville B
+ */  
+void commounLink_adding(uint a, uint b){
+ if((a < root.size()) && ( b < root.size()))
+	root[a].add_link(&root[b]);
+	root[b].add_link(&root[a]);
+}
+
+ /**
+ 	* @desc bloque le mutex de la prochaine ville et debloque le mutex de l'ancienne ville 
+ */  
+void mutex_securing(uint oldLocation, uint newLocation){
+	root[newLocation].set_arriving();
+	root[oldLocation].set_leaving();
+}
+
+ /**
+ 	* @desc bouge les voiture d'une ville A dans une ville B
+ */  
 void car_moving(uint id_car){
+
+	 // tant que la voiture à de l'essence on deplace les voitures
 	while(vehicules[id_car].get_gaz() > 0){
 		uint newLocation;
 		uint oldLocation = vehicules[id_car].get_location();
+
+		 // cherche une nouvelle ville à visiter autre que celle actuelle 
 		do{
 			newLocation = root[oldLocation].get_nodeVectorId(rand() % root[oldLocation].get_nodeVectorSize()); //on choisis un route random parmis depuis la ville
 		}while(newLocation == oldLocation);
@@ -81,12 +117,17 @@ void car_moving(uint id_car){
 			std::cout << " [DEMAND]la voiture " << id_car << " veut passer a  " << root[oldLocation].get_cityName() << " à " << root[newLocation].get_cityName() << std::endl;
 		global_mutex.unlock();
 
+		 // on appele les mutex pour eviter de tout casser
 		mutex_securing(oldLocation, newLocation);
+
+		 // on deplace notre voiture
 		vehicules[id_car].move(newLocation);
 
 		global_mutex.lock();
 			std::cout << "[SUCCESS]la voiture " << id_car << " passe de " << root[oldLocation].get_cityName() << " à " << root[newLocation].get_cityName() << std::endl;
 		global_mutex.unlock();
+		
+		// on attend entre 0 et 3 secondes
 		sleep(rand() % 3);
 	}
  	
